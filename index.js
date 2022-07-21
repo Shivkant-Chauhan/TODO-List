00a5ba43b38d09923ec15fab8010bfaf4b50fd08
Shivkant-Chauhan <shivkantchauhan2002@gmail.com>
2022-07-21 17:43:26 +0530

    added button functionalities

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+var number = 0;
+
 function main(){
     let task = document.querySelector(".data").value;
-
+    // if(task == ""){
+    //     document.getElementById("button-addon2").disabled = true;
+    // }
+    console.log(task);
 
     let list = document.createElement('li');
+    list.classList.add("num" + number);
+    number++;
     let node = document.createElement("div");
     node.classList.add("taskData")
     
@@ -21,14 +28,16 @@ function main(){
     i1.classList.add("fa-circle-check");
     i1.classList.add("i1");
     let att = document.createAttribute("onclick");
-    att.value = "icon1()";
+    att.value = "icon1(event)";
     i1.setAttributeNode(att);
 
     i2.classList.add("fa-solid");
     i2.classList.add("fa-lg");
     i2.classList.add("fa-pen-to-square");
     i2.classList.add("i2");
-    
+    att = document.createAttribute("onclick");
+    att.value = "icon2(event)";
+    i2.setAttributeNode(att);
 
 
     i3.classList.add("fa-solid");
@@ -36,7 +45,7 @@ function main(){
     i3.classList.add("fa-circle-xmark");
     i3.classList.add("i3");
     att = document.createAttribute("onclick");
-    att.value = "icon3(i3)";
+    att.value = "icon3(event)";
     i3.setAttributeNode(att);
 
 
@@ -48,7 +57,7 @@ function main(){
     list.appendChild(node);
     document.querySelector('ul').appendChild(list);
 
-
+// console.log(lists_arr);
     let field = document.querySelector(".data");
     field.value = "";
 
@@ -57,10 +66,11 @@ function main(){
 }
 
 
-$("#entry").keydown(function(e){
+$("#entry").keyup(function(e){
     let btn = document.getElementById("button-addon2");
     let z = document.querySelector(".data").value;
-    if(z === ""){
+    console.log(z);
+    if(z == "" || z == '\0'){
         // i have to press backspace 1 extra time to enter this condition!!
         btn.disabled = true;
     } else{
@@ -76,19 +86,32 @@ function clearTasks(){
 }
 
 
-
-// let icon1 = document.querySelector(".i1");
-// icon1.addEventListener("click", function(){
-//     let task = document.querySelector(".values");
-//     task.innerHTML = "<s>"+task.textContent+"</s>";
-// });
-
-function icon1(){
-    let task = document.querySelector(".values");
-    task.innerHTML = "<s>"+task.textContent+"</s>";
+function icon1(event){
+    var task = event.path[3];
+    task = task.querySelector(".values");
+    
+    var checker = "<s>" + task.textContent + "</s>";
+    if(task.innerHTML == checker){
+        task.innerHTML = task.textContent;
+    } else{
+        task.innerHTML = checker;
+    }
 }
 
+function icon2(event){
+    var initial = event.path[3];
+    initial = initial.querySelector(".values").innerHTML;
+    let newTask = "";
+    while(newTask == ""){
+        newTask = prompt("Modify your task !", initial);
+    }
 
-function icon3(className){
+    event.path[3].querySelector(".values").innerHTML = newTask;
     
+}
+
+function icon3(event){
+    var task = event.path[3];
+    task.innerHTML = "";
+    task.classList.add("none");
 }
